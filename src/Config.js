0d6e8ad5ec238f14b2add0cfcd0c088d200a4ed3
shimron <shimronduan@gmail.com>
2021-03-02 02:22:15 +0530

    config update

--- src/Config.js
+++ src/Config.js
@@ -1,3 +1,4 @@
-const baseUrl = "http://127.0.0.1:3000/api/v1";
+// const baseUrl = "http://127.0.0.1:3000/api/v1";
+const baseUrl = "https://secure-reaches-55392.herokuapp.com/";
 
 export { baseUrl };
